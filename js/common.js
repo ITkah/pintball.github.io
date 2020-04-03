9295f175c4858b6b27172ac1d2d178f2347fc6e5
ITkah <ilatkacenko132@gmail.com>
2020-04-04 00:59:26 +0300

    end

--- js/common.js
+++ js/common.js
@@ -1,5 +1,30 @@
 $(document).ready(function() {
 
+  $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+    $(this)
+      .addClass('active').siblings().removeClass('active')
+      .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+  });
+
+  $('.popup-with-form').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#name',
+
+		// When elemened is focused, some mobile browsers in some cases zoom in
+		// It looks not nice, so we disable it:
+		callbacks: {
+			beforeOpen: function() {
+				if($(window).width() < 700) {
+					this.st.focus = false;
+				} else {
+					this.st.focus = '#name';
+				}
+			}
+		}
+  });
+  
+
   $(".call-menu").on("click", function(){
     $("nav").slideToggle(250);
     $(".backdrop").toggleClass("backdrop-active");
